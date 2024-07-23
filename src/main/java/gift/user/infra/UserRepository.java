5619d333bfdd6242308369a5ff6d4a7e3b9b1c0d
stopmin <geemin0@naver.com>
2024-07-23 11:42:44 +0900

    feat: 카카오 로그인 유저 validation - 이미 존재하는 kakaoUser가 있는지 validation

--- src/main/java/gift/user/infra/UserRepository.java
+++ src/main/java/gift/user/infra/UserRepository.java
@@ -1,17 +1,9 @@
 package gift.user.infra;
 
 import gift.user.domain.User;
-
-import javax.swing.text.html.Option;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.Optional;
 
 
@@ -19,4 +11,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
+
+    Optional<Object> findByKakaoId(Long kakaoId);
 }
