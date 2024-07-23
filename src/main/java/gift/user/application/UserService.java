6f27f861243822b91288b18996701377a0c9508e
stopmin <geemin0@naver.com>
2024-07-23 11:41:08 +0900

    feat: 카카오 로그인 - 기존 register에서 카카오 로그인 회원 로직 확장

--- src/main/java/gift/user/application/UserService.java
+++ src/main/java/gift/user/application/UserService.java
@@ -1,12 +1,12 @@
 package gift.user.application;
 
 
+import gift.user.domain.LoginType;
 import gift.user.domain.User;
 import gift.user.domain.UserRegisterRequest;
 import gift.user.exception.UserException;
 import gift.user.infra.UserRepository;
 import gift.util.ErrorCode;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -33,14 +33,31 @@ public class UserService {
 
     @Transactional
     public User registerUser(UserRegisterRequest request) {
-        String encodedPassword = passwordEncoder.encode(request.getPassword());
-        // 중복회원 검증
-        validateDuplicateUser(request.getEmail());
+        if (request.getLoginType() == LoginType.NORMAL) {
+            // 일반 사용자 등록
+            String encodedPassword = passwordEncoder.encode(request.getPassword());
+            // 중복회원 검증
+            validateDuplicateUser(request.getEmail());
 
-        User user = new User();
-        user.setEmail(request.getEmail());
-        user.setPassword(encodedPassword);
-        return userRepository.save(user);
+            User user = new User();
+            user.setEmail(request.getEmail());
+            user.setPassword(encodedPassword);
+            user.setName(request.getName());
+            user.setLoginType(LoginType.NORMAL);
+            return userRepository.save(user);
+        } else if (request.getLoginType() == LoginType.KAKAO) {
+            // 카카오 사용자 등록
+            validateDuplicateKakaoUser(request.getKakaoId());
+
+            User user = new User();
+            user.setName(request.getName());
+            user.setProfileImageUrl(request.getProfileImageUrl());
+            user.setKakaoId(request.getKakaoId());
+            user.setLoginType(LoginType.KAKAO);
+            return userRepository.save(user);
+        } else {
+            throw new UserException(ErrorCode.INVALID_USER_TYPE);
+        }
     }
 
     private void validateDuplicateUser(String email) {
@@ -50,6 +67,15 @@ public class UserService {
                 }
         );
     }
+
+    private void validateDuplicateKakaoUser(Long kakaoId) {
+        userRepository.findByKakaoId(kakaoId).ifPresent(
+                user -> {
+                    throw new UserException(ErrorCode.DUPLICATE_USER);
+                }
+        );
+    }
+
 
     public User authenticateUser(String email, String password) {
         User user = userRepository.findByEmail(email).orElseThrow(
--- src/main/java/gift/user/domain/UserRegisterRequest.java
+++ src/main/java/gift/user/domain/UserRegisterRequest.java
@@ -1,9 +1,30 @@
 package gift.user.domain;
 
 public class UserRegisterRequest {
-
     private String email;
     private String password;
+    private String name;
+    private String profileImageUrl;
+    private Long kakaoId;
+    private LoginType loginType;
+
+    // 일반 사용자 생성자
+    public UserRegisterRequest(String email, String password, String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.loginType = LoginType.NORMAL;
+    }
+
+    // 카카오 사용자 생성자
+    public UserRegisterRequest(String name, String profileImageUrl, Long kakaoId) {
+        this.name = name;
+        this.profileImageUrl = profileImageUrl;
+        this.kakaoId = kakaoId;
+        this.loginType = LoginType.KAKAO;
+    }
+
+    // Getter 및 Setter
 
     public String getEmail() {
         return email;
@@ -20,4 +41,37 @@ public class UserRegisterRequest {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProfileImageUrl() {
+        return profileImageUrl;
+    }
+
+    public void setProfileImageUrl(String profileImageUrl) {
+        this.profileImageUrl = profileImageUrl;
+    }
+
+    public Long getKakaoId() {
+        return kakaoId;
+    }
+
+    public void setKakaoId(Long kakaoId) {
+        this.kakaoId = kakaoId;
+    }
+
+    public LoginType getLoginType() {
+        return loginType;
+    }
+
+    public void setLoginType(LoginType loginType) {
+        this.loginType = loginType;
+    }
 }
+
